b01a70556cedffa23b88811003b0b32159ceb9e7
TanzilaMokashi <mokashitanzila786@gmail.com>
2024-05-28 17:48:40 +0530

    RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -28,3 +28,50 @@ public class RewardValueTests {
         assert false;
     }
 }
+
+//New Class 
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.01;
+
+    // Constructor for cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Constructor for miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Getters
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
+
+    // Conversion methods
+    public double convertCashToMiles(double cash) {
+        return cash / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double convertMilesToCash(int miles) {
+        return miles * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Override toString() method for better readability
+    @Override
+    public String toString() {
+        return "RewardValue{" +
+            "cashValue=" + cashValue +
+            ", milesValue=" + milesValue +
+            "}";
+    }
+}
+
